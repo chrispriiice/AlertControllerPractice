ff122bf9689cb3356c89d24b0f12526272b40dfa
Chris Price <christopher.r.price@gmail.com>
2020-01-29 23:03:40 -0500

    Followed the instructions. Build succeeded, but the cell is not configured/wired properly.

--- AlertControllerPractice/ViewController.swift
+++ AlertControllerPractice/ViewController.swift
@@ -8,12 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var lists: [String] = ["The first item"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lists.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = lists[indexPath.row]
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            lists.remove(at: indexPath.row)
+        }
+        tableView.reloadData()
+    }
+    
+    @IBAction func addPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Add Content", message: "", preferredStyle: .alert)
+        alert.addTextField { (UITextField) in }
+        alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { (UIAlertAction) in
+            let content = alert.textFields![0] as UITextField
+            self.lists.append(content.text!)
+            self.tableView.reloadData()
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+
 
 
 }
